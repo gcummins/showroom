6cf5037f385bd0f2550d779d4dae10da75fbc66d
SA Pairing Station2 <sa-pair-2@Sources-MacBook-Pro-2.local>
2015-11-11 16:20:02 -0600

    Darin - Added some mdl styling to the tank create form

--- resources/views/tank/create.blade.php
+++ resources/views/tank/create.blade.php
@@ -1,9 +1,37 @@
 @extends('layout')
 
 @section('content')
-	<h1>New Tank</h1>
-	{!! Form::open(array('url'=>'tank'))!!}
-	@include('tank.form')
-	{!! Form::close() !!}
+	<!-- Wide card with share menu button -->
+	<style>
+		.create-card-wide.mdl-card {
+		margin: auto;
+		width: 512px;
+		}
+		.create-card-wide > .mdl-card__title {
+		color: #fff;
+		height: 100px;
+		background-color: indigo;
+		}
+		.create-card-wide > .mdl-card__menu {
+		color: #fff;
+		}
+	</style>
+
+	<div class="create-card-wide mdl-card mdl-shadow--2dp">
+		<div class="mdl-card__title">
+			<h2 class="mdl-card__title-text">New Tank</h2>
+		</div>
+		{!! Form::open(array('url'=>'tank'))!!}
+		<div class="mdl-card__supporting-text">
+			@include('tank.form')
+		</div>
+		<div class="mdl-card__actions mdl-card--border">
+			{!! Form::submit('Create Tank', array(
+			    'class' => "mdl-button mdl-button--colored mdl-js-button mdl-button--raised"
+			)) !!}
+		</div>
+		{!! Form::close() !!}
+
+	</div>
 
 @stop
--- resources/views/tank/form.blade.php
+++ resources/views/tank/form.blade.php
@@ -1,64 +1,193 @@
-{!! Form::label('status', 'Status') !!}
+<style>
+.mdl-textfield {
+    width: 100%;
+}
+</style>
+
+<div>
+{!! Form::label('status_id', 'Status') !!}
 {!! Form::select('status_id', $statuses) !!}
+</div>
 
-{!! Form::label('year', 'Year') !!}
-{!! Form::number('year') !!}
+<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+    {!! Form::label('year', 'Year', array(
+        'class'   => "mdl-textfield__label"
+    )) !!}
+    {!! Form::text('year', null, array(
+        'class'   => "mdl-textfield__input",
+        'pattern' => "^(19|20)\d\d"
+    )) !!}
+    <span class="mdl-textfield__error">Enter a valid year</span>
+</div>
 
-{!! Form::label('make', 'Make') !!}
-{!! Form::text('make') !!}
+<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+    {!! Form::label('make', 'Make', array('class' => 'mdl-textfield__label')) !!}
+    {!! Form::text('make', null, array('class' => 'mdl-textfield__input')) !!}
+</div>
 
-{!! Form::label('model', 'Model') !!}
-{!! Form::text('model') !!}
+<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+    {!! Form::label('model', 'Model', array(
+        'class'   => "mdl-textfield__label"
+    )) !!}
+    {!! Form::text('model', null, array(
+        'class'   => "mdl-textfield__input"
+    )) !!}
+</div>
 
-{!! Form::label('dotCode', 'D.O.T. Code') !!}
-{!! Form::text('dotCode') !!}
+<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+    {!! Form::label('dotCode', 'D.O.T. Code', array(
+        'class'   => "mdl-textfield__label"
+    )) !!}
+    {!! Form::text('dotCode', null, array(
+        'class'   => "mdl-textfield__input"
+    )) !!}
+</div>
 
-{!! Form::label('capacity', 'Capacity') !!}
-{!! Form::text('capacity') !!}
+<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+    {!! Form::label('capacity', 'Capacity', array(
+        'class'   => "mdl-textfield__label"
+    )) !!}
+    {!! Form::text('capacity', null, array(
+        'class'   => "mdl-textfield__input"
+    )) !!}
+</div>
 
-{!! Form::label('compartments', '# of Compartments') !!}
-{!! Form::number('compartments') !!}
+<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+    {!! Form::label('compartments', '# of Compartments', array(
+        'class'   => "mdl-textfield__label"
+    )) !!}
+    {!! Form::text('compartments', null, array(
+        'class'   => "mdl-textfield__input",
+        'pattern' => "-?[0-9]*(\.[0-9]+)?"
+    )) !!}
+    <span class="mdl-textfield__error">Enter a number</span>
+</div>
 
-{!! Form::label('bulkheads', 'Bulkheads') !!}
-{!! Form::text('bulkheads') !!}
+<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+{!! Form::label('bulkheads', 'Bulkheads', array(
+    'class'   => "mdl-textfield__label"
+)) !!}
+{!! Form::text('bulkheads', null, array(
+    'class'   => "mdl-textfield__input"
+)) !!}
+</div>
 
-{!! Form::label('compartmentSizes', 'Compartment Sizes') !!}
-{!! Form::text('compartmentSizes') !!}
+<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+{!! Form::label('compartmentSizes', 'Compartment Sizes', array(
+    'class'   => "mdl-textfield__label"
+)) !!}
+{!! Form::text('compartmentSizes', null, array(
+    'class'   => "mdl-textfield__input"
+)) !!}
+</div>
 
-{!! Form::label('pumps', 'Pumps') !!}
-{!! Form::text('pumps') !!}
+<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+{!! Form::label('pumps', 'Pumps', array(
+    'class'   => "mdl-textfield__label"
+)) !!}
+{!! Form::text('pumps', null, array(
+    'class'   => "mdl-textfield__input"
+)) !!}
+</div>
 
-{!! Form::label('pto', 'PTO Controls') !!}
-{!! Form::text('pto') !!}
+<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+{!! Form::label('pto', 'PTO Controls', array(
+    'class'   => "mdl-textfield__label"
+)) !!}
+{!! Form::text('pto', null, array(
+    'class'   => "mdl-textfield__input"
+)) !!}
+</div>
 
-{!! Form::label('meters', 'Meters') !!}
-{!! Form::text('meters') !!}
+<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+{!! Form::label('meters', 'Meters', array(
+    'class'   => "mdl-textfield__label"
+)) !!}
+{!! Form::text('meters', null, array(
+    'class'   => "mdl-textfield__input"
+)) !!}
+</div>
 
-{!! Form::label('spitTanks', 'Spit Tanks') !!}
-{!! Form::text('spitTanks') !!}
+<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+{!! Form::label('spitTanks', 'Spit Tanks', array(
+    'class'   => "mdl-textfield__label"
+)) !!}
+{!! Form::text('spitTanks', null, array(
+    'class'   => "mdl-textfield__input"
+)) !!}
+</div>
 
-{!! Form::label('hoseReels', 'Hose Reels') !!}
-{!! Form::text('hoseReels') !!}
+<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+{!! Form::label('hoseReels', 'Hose Reels', array(
+    'class'   => "mdl-textfield__label"
+)) !!}
+{!! Form::text('hoseReels', null, array(
+    'class'   => "mdl-textfield__input"
+)) !!}
+</div>
 
-{!! Form::label('deliveryHoses', 'Delivery Hoses') !!}
-{!! Form::text('deliveryHoses') !!}
+<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+{!! Form::label('deliveryHoses', 'Delivery Hoses', array(
+    'class'   => "mdl-textfield__label"
+)) !!}
+{!! Form::text('deliveryHoses', null, array(
+    'class'   => "mdl-textfield__input"
+)) !!}
+</div>
 
-{!! Form::label('nozzles', 'Nozzles') !!}
-{!! Form::text('nozzles') !!}
+<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+{!! Form::label('nozzles', 'Nozzles', array(
+    'class'   => "mdl-textfield__label"
+)) !!}
+{!! Form::text('nozzles', null, array(
+    'class'   => "mdl-textfield__input"
+)) !!}
+</div>
 
-{!! Form::label('dryBreakManifold', 'Dry Breaks/Manifold') !!}
-{!! Form::text('dryBreakManifold') !!}
+<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+{!! Form::label('dryBreakManifold', 'Dry Breaks/Manifold', array(
+    'class'   => "mdl-textfield__label"
+)) !!}
+{!! Form::text('dryBreakManifold', null, array(
+    'class'   => "mdl-textfield__input"
+)) !!}
+</div>
 
-{!! Form::label('bottomLoad', 'Bottom Load') !!}
-{!! Form::text('bottomLoad') !!}
+<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+{!! Form::label('bottomLoad', 'Bottom Load', array(
+    'class'   => "mdl-textfield__label"
+)) !!}
+{!! Form::text('bottomLoad', null, array(
+    'class'   => "mdl-textfield__input"
+)) !!}
+</div>
 
-{!! Form::label('vk', 'V-K') !!}
-{!! Form::text('vk') !!}
+<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+{!! Form::label('vk', 'V-K', array(
+    'class'   => "mdl-textfield__label"
+)) !!}
+{!! Form::text('vk', null, array(
+    'class'   => "mdl-textfield__input"
+)) !!}
+</div>
 
-{!! Form::label('ip', 'I-P') !!}
-{!! Form::text('ip') !!}
+<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+{!! Form::label('ip', 'I-P', array(
+    'class'   => "mdl-textfield__label"
+)) !!}
+{!! Form::text('ip', null, array(
+    'class'   => "mdl-textfield__input"
+)) !!}
+</div>
 
-{!! Form::label('comments', 'Comments') !!}
-{!! Form::textArea('comments') !!}
+<br>
 
-{!! Form::submit('Create Tank') !!}
+<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+{!! Form::label('comments', 'Comments', array(
+    'class'   => "mdl-textfield__label"
+)) !!}
+{!! Form::textArea('comments', null, array(
+    'class'   => "mdl-textfield__input",
+    'size'    => '200x8'
+)) !!}
+</div>
