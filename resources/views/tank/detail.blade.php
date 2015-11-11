7d3b501f1787db770127484d08d56050d4b7ea74
SA Pairing Station2 <sa-pair-2@Sources-MacBook-Pro-2.local>
2015-11-11 13:30:01 -0600

    Darin - Added photos to detail page and tested 1-2 photos

--- resources/views/tank/detail.blade.php
+++ resources/views/tank/detail.blade.php
@@ -1,1 +1,4 @@
 <h1>{{ $tank->year . " " . $tank->make . " " . $tank->capacity }}</h1>
+@foreach ($tank->photos as $photo)
+    <img src="{{ $photo->path }}">
+@endforeach
--- tests/TankCreationTest.php
+++ tests/TankCreationTest.php
@@ -33,8 +33,6 @@ class TankCreationTest extends TestCase
      */
     public function testThatTankDetailsAreViewable()
     {
-        $author = factory(App\User::class)->create();
-
         $tank = factory(App\Tank::class)->create();
 
         $this
@@ -56,5 +54,46 @@ class TankCreationTest extends TestCase
         $this
             ->visit('/tanks')
             ->see($tank->year . " " . $tank->make . " " . $tank->capacity);
+    }
+
+    public function testThatAPhotoCanBeAddedToATank()
+    {
+        $tank = factory(App\Tank::class)->create();
+        $photo = factory(App\Photo::class)->make();
+        $tank->photos()->save($photo);
+
+        $this->seeInDatabase('photos', ['tank_id' => $tank->id]);
+        $this->assertTrue($tank->photos->contains($photo->id));
+    }
+
+    public function testThatATankCanBeCreatedWithAPhoto()
+    {
+        //
+    }
+
+    public function testThatATankWithAPhotoShowsThePhotoOnDetailPage()
+    {
+        $tank = factory(App\Tank::class)->create();
+        $photo = factory(App\Photo::class)->make();
+        $tank->photos()->save($photo);
+
+        $this
+            ->visit('/tank/' . $tank->id)
+            ->see($photo->path);
+    }
+
+    public function testThatATankWithTwoPhotosShowsBothPhotosOnDetailPage()
+    {
+        $path1 = "photopath1.jpg";
+        $path2 = "photopath2.jpg";
+
+        $tank = factory(App\Tank::class)->create();
+        $tank->photos()->create(['path' => $path1]);
+        $tank->photos()->create(['path' => $path2]);
+
+        $this
+            ->visit('/tank/' . $tank->id)
+            ->see($path1)
+            ->see($path2);
     }
 }
